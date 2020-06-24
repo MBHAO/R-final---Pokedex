61e79193abe8bdf56dcef26bed3fa398f5955bcb
MBHAO <bill55171@gmail.com>
2020-06-24 19:52:59 +0800

    complete ... others to do:   make a battle

--- test.R
+++ test.R
@@ -1,7 +1,7 @@
 
 rm(list=ls())
 
-# 警告！　同時需要下載大量其他packages
+# 下載ggiraphExtra時, 同時需要下載大量的其他packages
 # also installing the dependencies ‘tinytex’, ‘xfun’, ‘highr’, 
 # ‘markdown’, ‘systemfonts’, ‘rlang’, ‘rmarkdown’, ‘knitr’, ‘base64enc’, 
 # ‘nortest’, ‘gdtools’, ‘uuid’, ‘insight’, ‘tidyselect’, ‘vctrs’, 
@@ -34,7 +34,6 @@ if (!require(DT)) {
   library(DT)
 }
 
-
 url <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vRNNywFlctSPG5Gi_q254S8VduxnTDiKtIxZUKUeDjW_2E5Lz1D4rgXs_I9d5WdVmqZb5Bd_wrWtIi3/pub?output=csv"
 
 poke_data <- read_csv(url)
@@ -46,11 +45,18 @@ poke_data <- read_csv(url)
 # 29:egg_type_1 蛋屬性一, 30:egg_type_2 蛋屬性二, 31:percentage_male 男性占比, 32:egg_cycles 蛋孵化週期
 poke_data <- poke_data[,-c(3, 7, 8, 11:16, 24:32)]
 
-show_pokedex <- function(data = poke_data, poke_property = "", tag = "", content = "", range = "=="){
 
-  if (poke_property == ""){
-    poke_property = 1:dim(data)[2]
-  }
+# tag 篩選的欄位(建議只用name抓, 但我沒擋其他欄位), content 篩選的內容
+get_pokemon <- function(tag, content){
+  return(poke_data[which(poke_data[,tag] == content),])
+}
+
+
+# data 資料, properties 想秀出的屬性, tag 篩選的欄位, content 篩選的內容, range 範圍
+show_pokedex <- function(data = poke_data, properties = "", tag = "", content = "", range = "=="){
+
+  suppressWarnings( if (properties == "") properties <- 1:dim(data)[2] )
+  condition = 1:dim(data)[1]
   
   if (tag != ""){
     if (range == "=="){
@@ -68,31 +74,38 @@ show_pokedex <- function(data = poke_data, poke_property = "", tag = "", content
     }
   }
   
-  data <- data[condition, poke_property]
+  data <- data[condition, properties]
   
   datatable(data, filter = "top", rownames = NULL)
 }
 
 
-
-a <- names(poke_data)[c(2,9:14)]
-
-
-origin <- poke_data[1, c(2, 9:14)]
-origin[1] <- ""
-origin[2:7] <- 0
-
-temp2 <- rbind(
-  origin,
-  poke_data[which(poke_data["name"] == "Mega Charizard X"), c(2, 9:14)],
-  poke_data[which(poke_data["name"] == "Mega Charizard Y"), c(2, 9:14)]
+# poke_1&2 兩隻寶可夢所有的屬性, properties 想秀出的屬性
+compare_two <- function(poke_1, poke_2, properties = 9:14){
+  
+  origin <- poke_data[1, c(2, properties)]
+  origin[1:length(origin)] <- 0
+  origin["name"] <- ""
+  
+  comp_data <- rbind(
+    origin,
+    poke_1[,c(2, properties)],
+    poke_2[,c(2, properties)]
   )
+  
+  ggRadar(data=comp_data, aes(group=name), rescale = F, interactive = T)
+}
 
-View(temp2)
 
-ggRadar(data=iris, aes(group=Species))
-ggRadar(data=temp2, aes(group=name), rescale = F, interactive = T, size = 2)
 
+
+# 測試結果
+
+show_pokedex(properties = c(1:14))
+
+poke_1 <- get_pokemon(tag = "name", content = "Mega Venusaur")
+poke_2 <- get_pokemon(tag = "name", content = "Mega Charizard X")
+compare_two(poke_1, poke_2)
 
 
 
